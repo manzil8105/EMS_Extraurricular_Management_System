72c5bca49eae8b0ceae62da06050e6f282d32934
Irin222 <138356191+Irin222@users.noreply.github.com>
2023-08-07 16:58:54 +0600

    i2

--- src/main/java/com/example/projectmain/Main.java
+++ src/main/java/com/example/projectmain/Main.java
@@ -15,6 +15,8 @@ public class Main extends Application {
         stage.setTitle("Primary homepage");
         stage.setScene(scene);
         stage.show();
+
+        // irin was here
     }
 
     public static void main(String[] args) {
